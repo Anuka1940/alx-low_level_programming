992937bfc73a3bb1dc7e9582b96b07ffaae08da4
Anuka1940 <Anukalateef@gmail.com>
2022-10-20 13:30:44 +0100

    _isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,17 @@
 /**
 *_isalpha -this check for alphabets only
-*@parameters:it accept one character int
+*@parameters:it accept one character intc
 *
 *Description: check if variable is either lowercase or upercase
 *
 *Return: return (1) for lowercase or upercase esle return (0)
 */
-int _isalpha(int c) 
+int_isalpha(intc)
+/*intc takes only one variable*/
 {
-    int i = c;
+int i = c;
 
-    if ((i >= 65 && i <= 90)||(i >=97 && i <= 122))
-        return (1);
-    else
-        return (0);
-}
+if ((i >= 65, &&i <= 90) || (i >= 97, &&i <= 122))
+return (1);
+else
+return (0)}
