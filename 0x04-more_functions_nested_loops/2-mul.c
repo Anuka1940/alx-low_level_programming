0d2b9b48d96a2d17d8e9892f3ea20ad503c147b4
Anuka1940 <Anukalateef@gmail.com>
2022-10-21 23:10:06 +0100

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+*mul(int a, int b)- multiple two variable a,b
+*
+*Return: Always return i
+*/
+int mul(int a, int b)
+{
+    int i;
+    i = a*b;
+
+    if ((a >= 48 && a <= 57) && (b >= 48 && a <= 57))      
+ 
+        return (i);
+}
