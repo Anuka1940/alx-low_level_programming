83579530d1f253f016dc4cdba13ce71a4b84d432
Anuka1940 <Anukalateef@gmail.com>
2022-10-27 17:41:24 +0100

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+int _isupper(int c);
+int mul(int a, int b);
+void print_most_numbers(void);
+int _putchar(char c);
+void more_numbers(void);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+char *_strcat(char *dest, char *src);
