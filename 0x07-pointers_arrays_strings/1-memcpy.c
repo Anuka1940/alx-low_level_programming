3ad02ce693621919479fb5fa9565cb87bb63f019
Anuka abdul lateef <Anukalateef@gmail.com>
2022-10-31 10:52:51 -0700

    corrected betty

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
-/*
+/**
  *_memcpy-  function that copies memory area
  *@dest: pointer to destination
  *@src: pointer to source
  *
  *Return: return dest always
- **/
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
