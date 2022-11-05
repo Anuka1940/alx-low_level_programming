4bc4f40a95af37ce74d2356e399d27cef5cc3745
Anuka abdul lateef <Anukalateef@gmail.com>
2022-11-05 13:37:44 -0700

    remove putchar function

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,4 @@
-#include <unistd.h>
 #include "main.h"
-/**
-*_putchar- fuction that output char and int
-*@c: the fuction accepts only one parameter of type char
-*
-*Return: int value always
-*/
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 /**
  *main- fuction that allow external inputs
  *@argc: it counts the number of arg inputed
