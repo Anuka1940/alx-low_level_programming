115fda148f9dc8b54a3f15b010a8e5fd795c340e
Anuka abdul lateef <Anukalateef@gmail.com>
2022-11-17 09:32:27 -0800

    how to define a structtype

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog- struct containing names,age and owner
+ *@name: member1
+ *@age: member2
+ *@owner: member3
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
