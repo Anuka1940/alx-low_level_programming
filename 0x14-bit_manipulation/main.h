955e2702d216d9b6423917a8de3d8593fde84400
Abdul Lateef Anuka <anukalateef@gmail.com>
2022-12-25 15:37:22 -0800

    updated

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,9 @@
 #define BINARY
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 unsigned int expo(unsigned int base, int power);
+void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 #endif /*(end BINARY) */
