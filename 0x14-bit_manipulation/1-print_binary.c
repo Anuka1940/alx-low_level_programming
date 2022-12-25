d5be44558e3482b8bf52c4b5eb2bb8f018dcb8df
Abdul Lateef Anuka <anukalateef@gmail.com>
2022-12-25 15:25:28 -0800

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+void print_binary(unsigned long int n)
+{
+        unsigned int b;
+        int count = 0, left_shift;
+        for (left_shift = 31; left_shift >= 0; left_shift--)
+        {
+                b = n >> left_shift;
+                if (b & 1)
+                {
+                        count = 1;
+                        printf("1");
+                }
+                else
+                {
+                        if (count == 1)
+                                printf("0");
+                }
+        }
+        if (count == 0)
+                printf("0");
+}
