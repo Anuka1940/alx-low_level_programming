6d3d831225b4b83592f3c017d23dd8a3c57a6794
Abdul Lateef Anuka <anukalateef@gmail.com>
2022-12-26 01:42:35 -0800

    print out index of a byte

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+/**
+ *get_bit-convert to binary
+ *@n:unsighned long int
+ *@index: size of index
+ *
+ *Return: 1 or 0 always
+ **/
+#include "main.h"
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int b;
+
+	b = n >> index;
+	if (b & 1)
+		return (1);
+	else
+		return (0);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@
 unsigned int expo(unsigned int base, int power);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
+int get_bit(unsigned long int n, unsigned int index);
 #endif /*(end BINARY) */
