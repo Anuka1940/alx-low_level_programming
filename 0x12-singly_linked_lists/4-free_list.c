8f6dfd80045dae5d48ab01aa32663579e7c64d9c
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-01-02 08:02:15 -0800

    free all nodes in a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *free_list- free all nodes in a list
+ *@head: pointer to the first node of list
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+	list_t *ptr;
+
+	while (temp != NULL)
+	{
+		ptr = temp;
+		temp = temp->next;
+		free(ptr);
+	}
+	head = NULL;
+}
+
