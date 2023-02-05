32e484acb750275a32b0e07c7a48d38bb9b80fb0
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-02-05 15:06:02 -0800

    remove 0 add NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,11 +12,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
-		return (0);
+		return (NULL);
 	table->size = size;
 	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
 	if (table == NULL)
-		return (0);
+		return (NULL);
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
