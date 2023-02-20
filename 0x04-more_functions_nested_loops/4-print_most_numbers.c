d2416f300fa84e124de3894095e13e47ac13b982
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-02-20 15:38:16 -0800

    print digit except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-
+/**
+ * print_most_numbers- print digits except 2 and 5
+ */
 void print_most_numbers(void)
 {
-    int i;
+	int i;
 
-    for (i = 48; i <= 57; i++)
-    {
-        if (i == 50 || i == 52)
-                continue;
-        else
-                _putchar (i);
-    }
-    _putchar (10);
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+			continue;
+		else
+			_putchar (i);
+	}
+	_putchar (10);
 }
