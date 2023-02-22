22edce0e342f3741c0b076ddfe2c6fcc9d46ff6e
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-02-22 04:27:18 -0800

    add space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,11 +21,11 @@ int main(void)
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
