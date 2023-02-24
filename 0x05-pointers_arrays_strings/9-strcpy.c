602667643300739c68877c43f1b31652d79abbbd
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-02-24 03:52:15 -0800

    copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy- copy a string
+ * @dest: pointer the copied string
+ * @src: pointer to the string to be copied
+ *
+ * Return: pointer dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
