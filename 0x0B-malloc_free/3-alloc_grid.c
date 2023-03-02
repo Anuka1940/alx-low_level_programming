05a0d47691117ce002e75623b3a7a88c36a7e6b4
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-03-02 02:11:18 -0800

    allocate memory

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * alloc_grid- allocate memory to each elements of an array
+ * @width: the matrix column
+ * @height: the matrix row
+ *
+ * Return: a pointer a an array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j = 0;
+	int **array;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = malloc(width * sizeof(int));
+	if (array == NULL)
+		return (NULL);
+
+	while (j < width)
+	{
+		for (i = 0; i < width; i++)
+		{
+			array[i] = malloc(height * sizeof(int));
+		}
+		j++;
+	}
+	return (array);
+}
