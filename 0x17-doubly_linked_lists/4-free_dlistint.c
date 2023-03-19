435aba41faa24080670af642df860a32a0189390
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-03-18 17:12:31 -0700

    free double link list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint- free a double link list
+ * @head: pointer to head of a link list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr;
+
+	while(head)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+}
